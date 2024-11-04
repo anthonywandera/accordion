7cc93fe1619d1267a9c31141212e91b7df2c325d
anthonywandera <wanderatony92@gmail.com>
2024-11-04 14:59:19 +0300

    final update

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 "use strict";
 
 const items = document.querySelectorAll(".item");
+const icons = document.querySelectorAll(".icon");
 const accordion = document.querySelector(".accordion");
 
 // open item on click
 function openItem(e) {
-  if (!e.target.classList.contains("text")) return;
+  if (
+    !e.target.classList.contains("icon") &&
+    !e.target.classList.contains("text")
+  )
+    return;
+
   items.forEach((item) => {
+    if (item === e.target.closest(".item")) return;
     item.classList.remove("open");
   });
 
